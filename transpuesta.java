3774d64224f9a3d77ef74e2afb1c8bfbfe8cd3b0
FelixSabugal <fsabugalc03@educarex.es>
2024-05-23 13:00:57 +0200

    Add files via upload

---
+++ transpuesta.java
@@ -0,0 +1,86 @@
+package Examen;
+import java.util.Scanner;
+
+public class Transpuesta {
+    
+        static final int N=4;
+    
+   
+    static void transpose(int A[][], int B[][]) {
+        int i,j; 
+        for (i = 0; i < N; i++)
+            for (j = 0; j < N; j++)
+                B[i][j] = A[j][i];
+    }
+    
+    /*
+         static void transpose(int A[][], int B[][]) {
+        int i, j;
+        for (i = 0; i <= N; i++)
+            for (j = 0; j < N; j++)
+                B[i][j] = A[j+1][i-1];
+    }
+     */
+ 
+    public static void main(String[] args) {
+        int A[][] = {{1, 1, 1, 1},
+                {2, 2, 2, 2},
+                {3, 3, 3, 3},
+                {4, 4, 4, 4}};
+ 
+        int B[][] = new int[N][N],i,j;
+        System.out.println("Transpuesta de una matriz:\n");
+ 
+        System.out.println("\nAntes de  Transponer:");
+ 
+        Mostrar(A);
+ 
+        transpose(A,B);
+ 
+        System.out.println("\nDespués de  Transponer:");
+        Mostrar(B); 
+    }
+ 
+    /*    public static void main(String[] args) {
+        int A[][] = {{1, 1, 1, 1},
+                {2, 2, 2, 2},
+                {3, 3, 3, 3},
+                {4, 4, 4, 4}};
+ 
+        int B[][] = new int[N][N], i, j;
+        System.out.println("Transpuesta de una matriz:\n");
+ 
+        System.out.println("\nAntes de  Transponer:");
+ 
+        Mostrar(A);
+ 
+        transpose(A, B);
+ 
+        System.out.println("\nDespués de  Transponer:");
+        Mostrar(B);
+    } */
+    
+    private static void Mostrar(int[][] A) {
+        int i;
+        int j;
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++)
+                System.out.print(A[i][j] + " ");
+            System.out.print("\n");
+        }
+    }
+    
+    /*
+     *     private static void Mostrar(int[][] b) {
+        int i;
+        int j;
+        for (i = 0; i < N; i++) {
+            for (j = 0; j <= N; j++)
+                System.out.print(b[i][j] + " ");
+            System.out.print("\n");
+        }
+    }
+     */
+}
+
+
